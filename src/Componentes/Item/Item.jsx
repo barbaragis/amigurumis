8d60eea0fa4a9b562a182efb17dea6343948c8ac
Barbara Gissi <gissibarbara@gmail.com>
2023-09-30 13:16:07 -0300

    arreglos

--- src/Componentes/Item/Item.jsx
+++ src/Componentes/Item/Item.jsx
@@ -12,14 +12,11 @@ export const Item = ({
   const navigate= useNavigate();
     return(
       <div className="item"  onClick={() => navigate(`/item/${id}`)}>
-        <div className="producto__card">
           <img src={img} class="producto__imagen" alt="..."/>
-        <div className="card-body">
           <h5 className="producto__titulo">{title}</h5>
           <p className="producto__categoria">{category}</p>
           <button  className="producto__boton">Ver detalle</button>
         </div>
-        </div>
-    </div>
+
     )
 }
